e2851fea6286cce67417c581d62058f4f0e17697
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2010-03-21 15:14:47 +0000

    fixed build.sh
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@119 83beac0b-88d4-de11-98f5-001125a849b0

--- build.sh
+++ build.sh
@@ -1,35 +1,45 @@
 #!/bin/sh
 
-echo "#### FOR TESTING ONLY !!! ####"
+echo "###########################################################"
+echo "########## build.sh is FOR DEVELOPMENT ONLY !!! ###########"
+echo "###########################################################"
+echo ""
+echo "to build from svn, run 'aclocal', 'automake --add-missing',"
+echo "'autoconf' - followed by plain old './configure', 'make'"
+echo "and 'make install'."
+echo ""
+echo "###########################################################"
+echo "####### please report any bug to <bugs@c0decafe.de> #######"
+echo "###########################################################"
+
+read
 
 #Build Lib
 gcc -c -o lib/md5.o lib/md5.c -fpic -Wall
 
 #Build ASLEAP
-cd lib/asleap
-make
-cd ../..
+#cd lib/asleap
+#make
+#cd ../..
 
 #Build TCPMD5 Modules
 gcc -c -o loki/tcpmd5/tcpmd5.o loki/tcpmd5/tcpmd5.c `python-config --cflags` -fpic -Wall
 gcc -c -o loki/tcpmd5/tcpmd5bf.o loki/tcpmd5/tcpmd5bf.c `python-config --cflags` -fpic -Wall -I.
 ld -shared -soname tcpmd5.so loki/tcpmd5/tcpmd5.o -o loki/tcpmd5/tcpmd5.so -lc
 ld -shared -soname tcpmd5bf.so loki/tcpmd5/tcpmd5bf.o lib/md5.o -o loki/tcpmd5/tcpmd5bf.so -lc
-strip loki/tcpmd5/tcpmd5.so
-strip loki/tcpmd5/tcpmd5bf.so
 
 #Build OSPF BF Module
 gcc -c -o loki/ospfmd5/ospfmd5bf.o loki/ospfmd5/ospfmd5bf.c `python-config --cflags` -fpic -Wall -I.
 ld -shared -soname ospfmd5bf.so loki/ospfmd5/ospfmd5bf.o lib/md5.o -o loki/ospfmd5/ospfmd5bf.so -lc
-strip loki/ospfmd5/ospfmd5bf.so
 
 #Build ASLEAP Module
-gcc -g -c -o lib/asleap/asleap.o lib/asleap/asleap.c -fpic -Wall -I./lib/asleap 
-gcc -g -c -o loki/asleap/asleap.o loki/asleap/asleap.c `python-config --cflags` -fpic -Wall -I.
-ld -shared -soname asleap.so loki/asleap/asleap.o lib/asleap/common.o lib/asleap/asleap.o lib/asleap/utils.o lib/asleap/sha1.o -o loki/asleap/asleap.so -lc -lpcap -lcrypt -lcrypto
-strip loki/asleap/asleap.so
+gcc -c -o loki/asleap/common.o lib/asleap/common.c -I./lib/asleap -fpic -DHAVE_OPENSSL_MD4_H
+gcc -c -o loki/asleap/utils.o lib/asleap/utils.c -I./lib/asleap -fpic
+gcc -c -o loki/asleap/sha1.o lib/asleap/sha1.c -I./lib/asleap -fpic -DHAVE_ENDIAN_H
+gcc -c -o loki/asleap/bin_asleap.o lib/asleap/asleap.c -I./lib/asleap -fpic
+gcc -c -o loki/asleap/asleap.o loki/asleap/asleap.c `python-config --cflags` -fpic -Wall -I.
+ld -shared -soname asleap.so loki/asleap/asleap.o loki/asleap/common.o loki/asleap/utils.o loki/asleap/sha1.o loki/asleap/bin_asleap.o -o loki/asleap/asleap.so -lc -lpcap -lcrypt -lcrypto -lssl
 
 #Build SHA1 Module
-gcc -g -c -o loki/sha1/sha1_prf.o loki/sha1/sha1_prf.c `python-config --cflags` -fpic -Wall -I.
+gcc -c -o loki/sha1/sha1_prf.o loki/sha1/sha1_prf.c `python-config --cflags` -fpic -Wall -I.
 ld -shared -soname sha1_prf.so loki/sha1/sha1_prf.o  -o loki/sha1/sha1_prf.so -lssl
-strip loki/sha1/sha1_prf.so
