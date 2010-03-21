5d910336367d9c0e191eb1b0ff7ba3fc5a55e0b7
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2010-03-21 16:53:51 +0000

    minor changes
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@122 83beac0b-88d4-de11-98f5-001125a849b0

--- build.sh
+++ build.sh
@@ -33,10 +33,10 @@ gcc -c -o loki/ospfmd5/ospfmd5bf.o loki/ospfmd5/ospfmd5bf.c `python-config --cfl
 ld -shared -soname ospfmd5bf.so loki/ospfmd5/ospfmd5bf.o lib/md5.o -o loki/ospfmd5/ospfmd5bf.so -lc
 
 #Build ASLEAP Module
-gcc -c -o loki/asleap/common.o lib/asleap/common.c -I./lib/asleap -fpic -DHAVE_OPENSSL_MD4_H
-gcc -c -o loki/asleap/utils.o lib/asleap/utils.c -I./lib/asleap -fpic
-gcc -c -o loki/asleap/sha1.o lib/asleap/sha1.c -I./lib/asleap -fpic -DHAVE_ENDIAN_H
-gcc -c -o loki/asleap/bin_asleap.o lib/asleap/asleap.c -I./lib/asleap -fpic
+gcc -c -o loki/asleap/common.o lib/asleap/common.c -I./lib/asleap -fpic -DHAVE_OPENSSL_MD4_H -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE
+gcc -c -o loki/asleap/utils.o lib/asleap/utils.c -I./lib/asleap -fpic -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE
+gcc -c -o loki/asleap/sha1.o lib/asleap/sha1.c -I./lib/asleap -fpic -DHAVE_ENDIAN_H -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE
+gcc -c -o loki/asleap/bin_asleap.o lib/asleap/asleap.c -I./lib/asleap -fpic -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE
 gcc -c -o loki/asleap/asleap.o loki/asleap/asleap.c `python-config --cflags` -fpic -Wall -I.
 ld -shared -soname asleap.so loki/asleap/asleap.o loki/asleap/common.o loki/asleap/utils.o loki/asleap/sha1.o loki/asleap/bin_asleap.o -o loki/asleap/asleap.so -lc -lpcap -lcrypt -lcrypto -lssl
 
