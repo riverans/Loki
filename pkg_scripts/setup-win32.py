3544a23ca521d56cb423c4b9a60e3313501c6fd4
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2012-11-08 08:14:58 +0000

    switched to gui app
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@324 83beac0b-88d4-de11-98f5-001125a849b0

--- pkg_scripts/setup-win32.py
+++ pkg_scripts/setup-win32.py
@@ -12,18 +12,9 @@ options = {
   'build_exe': {
     'includes': [ 'gtk.keysyms', 'dumbdbm', 'dbhash', 'new', 'numbers',
                   'hashlib', 'gtk.glade', 'hmac', 'IPy', 'dnet' ],
-    'base': 'Console',
-    'include_files': [ 'modules' ],
-#    'bin_excludes': [
-#                 'iconv.dll', 'intl.dll', 'libatk-1.0-0.dll',
-#                 'libgdk_pixbuf-2.0-0.dll', 'libgdk-win32-2.0-0.dll',
-#                 'libgio-2.0-0.dll',
-#                 'libglib-2.0-0.dll', 'libgmodule-2.0-0.dll',
-#                'libgobject-2.0-0.dll', 'libgthread-2.0-0.dll',
-#                 'libgtk-win32-2.0-0.dll', 'libpango-1.0-0.dll',
-#                 'libpangowin32-1.0-0.dll', 'libcairo-2.dll',
-#                 'libpangocairo-1.0-0.dll', 'libpangoft2-1.0-0.dll',
-#                 ]
+#    'base': 'Console',
+    'base': 'Win32GUI',
+    'include_files': [ 'modules' ]
     }
   }
 
