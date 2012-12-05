f65d0669960c26e7a9c78a7a76a6eccf29780b8b
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2012-12-05 22:21:27 +0000

    moved win32 build files
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@345 83beac0b-88d4-de11-98f5-001125a849b0

---
+++ setup-win32_exe.py
@@ -0,0 +1,27 @@
+from cx_Freeze import setup, Executable
+import glob
+import sys
+import os
+
+options = {
+  'build_exe': {
+    'includes': [ 'gtk.keysyms', 'dumbdbm', 'dbhash', 'new', 'numbers',
+                  'hashlib', 'gtk.glade', 'hmac', 'IPy', 'dnet' ],
+#    'base': 'Console',
+    'base': 'Win32GUI',
+    'include_files': [ 'modules' ]
+    }
+  }
+
+setup(
+  name='Loki',
+  version='0.2.7',
+  description='Loki',
+  author='Daniel Mende',
+  url='http://codecafe.de/loki.html',
+  license='GPL',
+  options=options,
+  executables=[Executable('src/loki.py')],
+) 
+
+os.system("mt.exe -manifest pkg_scripts\\loki.exe.manifest -outputresource:\"build\\exe.win32-2.6\\loki.exe;#1\"")
