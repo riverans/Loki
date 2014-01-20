639780b53c08badbedb27f2474db393faaa2553e
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2014-01-20 15:04:15 +0000

    added HMAC-MD5 to lib
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@369 83beac0b-88d4-de11-98f5-001125a849b0

---
+++ lib/hmac_md5.h
@@ -0,0 +1,24 @@
+/*
+ * hmac_md5.h
+ */
+
+#ifndef hmac_md5_INCLUDED
+#define hmac_md5_INCLUDED
+
+#ifndef ARCH_IS_BIG_ENDIAN
+#define ARCH_IS_BIG_ENDIAN 0 
+#endif
+#include "md5.h"
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+void hmac_md5(unsigned char* text, int text_len, unsigned char* key, int key_len, md5_byte_t* digest);
+
+#ifdef __cplusplus
+}  /* end extern "C" */
+#endif
+
+#endif /* hmac_md5_INCLUDED */
