3a734a26562db55384286783be84992c65c7aa93
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2014-12-16 15:18:44 +0000

    fixed include path
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@441 83beac0b-88d4-de11-98f5-001125a849b0

--- src/pppoe-tunnel.c
+++ src/pppoe-tunnel.c
@@ -33,7 +33,7 @@
  *      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "pppoetun.h"
+#include "lib/pppoetun.h"
 
 #define LOCKFILE_LENGTH 256
 char lockfile[LOCKFILE_LENGTH];
