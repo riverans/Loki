173dfd4531b595c6c61654be5fe4880108887839
greif <greif@83beac0b-88d4-de11-98f5-001125a849b0>
2015-01-15 11:28:55 +0000

    added context manager to hide output
    git-svn-id: http://c0decafe.de/svn/codename_loki/trunk@449 83beac0b-88d4-de11-98f5-001125a849b0

--- src/loki.py
+++ src/loki.py
@@ -61,6 +61,30 @@ DATA_DIR="."
 #~ For OSX Bundeling
 #~ DATA_DIR=os.path.expandvars("$bundle_data/loki")
 
+class HideOutput(object):
+    '''
+    A context manager that block stdout for its scope, usage:
+
+    with HideOutput():
+        os.system('ls -l')
+    '''
+
+    def __init__(self, *args, **kw):
+        sys.stdout.flush()
+        self._origstdout = sys.stdout
+        self._oldstdout_fno = os.dup(sys.stdout.fileno())
+        self._devnull = os.open(os.devnull, os.O_WRONLY)
+
+    def __enter__(self):
+        self._newstdout = os.dup(1)
+        os.dup2(self._devnull, 1)
+        os.close(self._devnull)
+        sys.stdout = os.fdopen(self._newstdout, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self._origstdout
+        sys.stdout.flush()
+        os.dup2(self._oldstdout_fno, 1)
 
 class pcap_thread(threading.Thread):
     def __init__(self, parent, interface):
